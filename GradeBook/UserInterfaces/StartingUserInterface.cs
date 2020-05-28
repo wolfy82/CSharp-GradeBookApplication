e535453c1ed5a5d617bb2fc59a0c437c4e95e130
Aaron Roth <aarongroth@gmail.com>
2020-05-28 16:12:10 -0700

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[3];
+            var type = parts[2];
             BaseGradeBook gradeBook;
             if (type == "standard")
             {
